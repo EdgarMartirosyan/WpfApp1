2f8b857a2e6affb445d39edcec3d7cafd84ef179
Edgar <edgar.martirosyan.a@gmail.com>
2019-09-22 22:48:15 +0400

    Add project files.

---
+++ WpfApp1/ComicProcessor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class ComicProcessor
+
+    {
+        //public int MaxComicNumber { get; set; }
+
+        public async static Task<ComicModel> LoadCimic(int comicNumber = 0)
+        {
+            string url = "";
+            if (comicNumber>0)
+            {
+                url = $"https://xkcd.com/ {comicNumber} /info.0.json"; 
+            }
+            else
+            {
+                url = "https://xkcd.com/info.0.json";
+            }
+
+            using (HttpResponseMessage response=await APIHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    ComicModel comic = await response.Content.ReadAsAsync<ComicModel>();
+
+                   /* if (comicNumber==0)
+                    {
+                        MaxComicNumber = comic.Num;
+                        
+                    }*/
+                    return comic;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }  
+    }
+}
